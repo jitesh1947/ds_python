cfc893cdae02266ab427654636165ac95ea194d0
jitesh1947 <jitesh.sprinter@gmail.com>
2020-05-19 15:52:12 +0100

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -6,7 +6,6 @@ for i in range(len(A)):
     for j in range(i+1, len(A)): 
         if A[min_idx] > A[j]: 
             min_idx = j         
-    # Swap the found minimum element with  
-    # the first element         
+    # Swap the found minimum element with the first element         
     A[i], A[min_idx] = A[min_idx], A[i] 
 print(A)
